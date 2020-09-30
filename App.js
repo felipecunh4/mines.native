7800c4ec4bc95eaed8d1ca770b77232930571b0b
Felipe Cunha <felipe.cunha@afya.com.br>
2020-09-29 21:20:31 -0300

    level selection component

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {StyleSheet, View, Alert} from 'react-native';
 import params from './src/params';
 import MineField from './src/components/MineField';
 import Header from './src/components/Header';
+import LevelSelection from './src/components/LevelSelection';
 import {
   createMinedBoard,
   cloneBoard,
@@ -75,6 +76,11 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <LevelSelection
+          isVisible={this.state.showLevelSelection}
+          onLevelSelected={this.onLevelSelected}
+          onCancel={() => this.setState({showLevelSelection: false})}
+        />
         <Header
           flagsLeft={this.minesAmount() - flagsUsed(this.state.board)}
           onNewGame={() => this.setState(this.createState())}
